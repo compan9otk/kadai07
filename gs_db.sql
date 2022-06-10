b29fe6714150e2bbfc68b23e33215d6e5b58bb9e
compan9otk <6ri6rikatatu6ri@gmail.com>
2022-06-11 04:40:22 +0900

    first commit

---
+++ gs_db.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost:8889
+-- 生成日時: 2022 年 6 月 10 日 19:38
+-- サーバのバージョン： 5.7.34
+-- PHP のバージョン: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE `gs_an_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `email`, `content`, `date`) VALUES
+(1, '大塚亮介', 'test@hoge.com', 'お問合せ', '2022-05-21 15:12:09'),
+(2, 'いぬ', 'inu@fuga.com', 'わん', '2022-05-21 15:16:01'),
+(3, 'ねこ', 'neko@fuga.com', 'にゃー', '2022-05-21 15:16:49'),
+(4, 'かに', 'cancer@test.com', 'test', '2022-05-21 16:27:11'),
+(5, 'sql_injection', 'test@hoge.com', '<script>alert(\'test\')</script>', '2022-05-21 17:23:06'),
+(6, 'otk', 'rakuten20210308@gmail.com', 'lalala', '2022-05-30 02:36:59'),
+(7, 'test0606', '', 'ｓｓｓ', '2022-06-06 04:52:29');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `url` text COLLATE utf8_unicode_ci NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_bm_table`
+--
+
+INSERT INTO `gs_bm_table` (`id`, `name`, `url`, `comment`, `date`) VALUES
+(1, 'name1', 'url1', 'comment1', '2022-06-11 04:36:42');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- テーブルの AUTO_INCREMENT `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
